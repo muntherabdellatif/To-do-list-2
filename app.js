1700ee130e4d5414173589006eb473eac8d021a0
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-11-13 13:37:10 +0200

    adding the default tasks to data base and read it

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express =require("express");
+const mongoose =require("mongoose");
 let bodyParser =require("body-parser");
 let cors =require("cors");
 let path =require("path");
@@ -7,6 +8,28 @@ let expressLayouts =require("express-ejs-layouts");
 const app =express();
 let taskList=[];
 let workTaskList=[];
+
+mongoose.connect("mongodb://localhost:27017/ToDoListDB",{useNewUrlParser:true});
+
+const taskListSchema = new mongoose.Schema({
+    name:{
+        type:String ,
+        required : [true] 
+    },
+});
+const TaskList =mongoose.model("tasklist",taskListSchema);
+
+// default tasks
+const task1 = new TaskList ({
+    name: "Steel sport"
+});
+const task2 = new TaskList ({
+    name: "Learn something new"
+});
+const task3 = new TaskList ({
+    name: "Spend some time with the family"
+});
+let defaultTasks =[task1,task2,task3];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
@@ -17,18 +40,36 @@ app.set("views",path.join(__dirname,"views")); // set the file include ejs
 app.set("view engine","ejs");
 
 app.get("/",function (req,res) {
+
+    TaskList.find(function (error,tasks) {
+        if (error){
+            console.log(error);
+        }else {
+            if (tasks.length===0){
+                TaskList.insertMany(defaultTasks,function (err) {
+                    if (err){console.log(err);}
+                });
+            }else {
+                taskList=[];
+                tasks.forEach((e)=>{
+                    taskList.push(e.name);
+                });
+            }
+        }
+    });
+    if (taskList.length===0){
+        res.redirect("/");
+    }
+    // time data
     let today =new Date();
-    let hour =today.getHours();
-    let dayHalf= "AM"
-    if (hour >12){ hour -=12; timeInDay="PM";
-    }else{ timeInDay="AM";}
-    let min =today.getMinutes();
     let dayInMonth =today.getDate();
-    let month =today.getMonth();
+    let month =today.getMonth()+1;
     let year =today.getFullYear();
     let weekDays =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day=weekDays[today.getDay()];
     let listTitle= `${day} To do list (${dayInMonth}/${month}/${year})`;
+
+    // render
     res.render("index",{
         taskList:taskList ,
         listTitle:listTitle
@@ -40,11 +81,9 @@ app.post("/",function(req,res){
     if (req.body.button===" Work To do list"){
         workTaskList.push(task);
         res.redirect("/work");
-        console.log("ok")
     }else {
         taskList.push(task);
         res.redirect("/");
-        console.log(":(");
     }
 });
 app.get("/work",function(req,res){
