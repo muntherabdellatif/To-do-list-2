75ef11f6d4badc8302ecffa0406c1dacc31dab8b
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-11-17 20:42:20 +0200

    adding pages to toDoList

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ let path =require("path");
 let expressLayouts =require("express-ejs-layouts");
 
 const app =express();
-let taskList=[];
-let workTaskList=[];
+let pagesTitles=[];
+let currentPage=1;
+let pages = [
+    {
+        pageTitle:"To do list page 1",
+        tasks:[
+            "task1" ,
+            "task2"
+        ]
+    },
+    {
+        pageTitle:"To do list page 2",
+        tasks:[
+            "task3" ,
+            "task4"
+        ]
+    }
+];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
@@ -17,45 +33,42 @@ app.set("views",path.join(__dirname,"views")); // set the file include ejs
 app.set("view engine","ejs");
 
 app.get("/",function (req,res) {
-    let today =new Date();
-    let hour =today.getHours();
-    let dayHalf= "AM"
-    if (hour >12){ hour -=12; timeInDay="PM";
-    }else{ timeInDay="AM";}
-    let min =today.getMinutes();
-    let dayInMonth =today.getDate();
-    let month =today.getMonth();
-    let year =today.getFullYear();
-    let weekDays =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let day=weekDays[today.getDay()];
-    let listTitle= `${day} To do list (${dayInMonth}/${month}/${year})`;
-    res.render("index",{
-        taskList:taskList ,
-        listTitle:listTitle
-    });   // read ejs file
-});
-app.post("/",function(req,res){
-    console.log(req.body.button);
-    let task =req.body.task;
-    if (req.body.button===" Work To do list"){
-        workTaskList.push(task);
-        res.redirect("/work");
-        console.log("ok")
-    }else {
-        taskList.push(task);
-        res.redirect("/");
-        console.log(":(");
+    if (pagesTitles.length!==pages.length) {
+        pagesTitles=[];
+        for(i=0;i<pages.length;i++){
+            pagesTitles.push(pages[i].pageTitle);
+        }
     }
-});
-app.get("/work",function(req,res){
     res.render("index",{
-        taskList:workTaskList ,
-        listTitle:" Work To do list"
+        currentpageTitle:pages[currentPage].pageTitle,
+        currentPageTasks:pages[currentPage].tasks,
+        pagesTitles:pagesTitles
     });   // read ejs file
 });
-app.post("/work",function(req,res){
-    
+app.post("/addtask",function(req,res){
+    let addToPage = req.body.button;
+    for(var n of pages){
+        if(n.pageTitle===addToPage){
+            n.tasks.push(req.body.task);
+        }
+    }
+    res.redirect("/");
+});
+app.post("/addPage",function(req,res){
+    let newPage={
+            pageTitle:req.body.page,
+            tasks:[]
+        };
+    pages.push(newPage);
+    res.redirect("/");
+});
+app.get(`/changePage/:postID`,function (req,res) {
+    currentPage = req.params.postID;
+    res.redirect("/");
+  });
+app.post("/",function(req,res){
+
 });
 app.listen(process.env.PORT || 3000,function () {
     console.log("running");  
-})
+});
