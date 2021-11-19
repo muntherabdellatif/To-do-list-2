3503b124172ac922cc4fc5bad4b353048cb209b7
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-11-19 19:07:15 +0200

    adding and reading pages and tasks to and from data base

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const express =require("express");
 let bodyParser =require("body-parser");
+const mongoose =require("mongoose");
 let cors =require("cors");
 let path =require("path");
 let expressLayouts =require("express-ejs-layouts");
+mongoose.connect("mongodb://localhost:27017/ToDoListDB",{useNewUrlParser:true});
+
+const PageTasksSchema = new mongoose.Schema({
+    taskName : String 
+});
+const pageTasks =mongoose.model("pageTasks",PageTasksSchema);
+
+const PagesListSchema = new mongoose.Schema({
+    _id : Number ,
+    name:String,
+    tasksList: [{type:PageTasksSchema}]
+});
+const PagesList =mongoose.model("PagesList",PagesListSchema);
 
 const app =express();
 let pagesTitles=[];
-let currentPage=1;
-let pages = [
-    {
-        pageTitle:"To do list page 1",
-        tasks:[
-            "task1" ,
-            "task2"
-        ]
-    },
-    {
-        pageTitle:"To do list page 2",
-        tasks:[
-            "task3" ,
-            "task4"
-        ]
+let currentPage=0;
+
+let pages = [];
+
+//find element in data base and push it in pages array 
+PagesList.find(function (error,ToDoListData) {
+    if (error){
+        console.log(error);
+    }else {
+        if (ToDoListData){
+            ToDoListData.forEach((page)=>{
+                console.log(page.name);
+                console.log(page.tasksList);
+                const newPage = {
+                    pageTitle :page.name ,
+                    tasks :page.tasksList
+                }
+                pages.push(newPage);
+                pagesTitles.push(page.name );
+            })
+        }
     }
-];
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
@@ -33,25 +53,44 @@ app.set("views",path.join(__dirname,"views")); // set the file include ejs
 app.set("view engine","ejs");
 
 app.get("/",function (req,res) {
-    if (pagesTitles.length!==pages.length) {
-        pagesTitles=[];
-        for(i=0;i<pages.length;i++){
-            pagesTitles.push(pages[i].pageTitle);
-        }
+    if (pages.length===0){
+        res.render("index",{
+            currentpageTitle:"welcome in To Do List",
+            currentPageTasks:[],
+            pagesTitles:[] 
+        });   // read ejs file
+    }else{
+        res.render("index",{
+            currentpageTitle:pages[currentPage].pageTitle,
+            currentPageTasks:pages[currentPage].tasks,
+            pagesTitles:pagesTitles 
+        });   // read ejs file
     }
-    res.render("index",{
-        currentpageTitle:pages[currentPage].pageTitle,
-        currentPageTasks:pages[currentPage].tasks,
-        pagesTitles:pagesTitles
-    });   // read ejs file
 });
 app.post("/addtask",function(req,res){
     let addToPage = req.body.button;
     for(var n of pages){
         if(n.pageTitle===addToPage){
-            n.tasks.push(req.body.task);
+            n.tasks.push({taskName:req.body.task});
         }
     }
+
+    // adding tasks to data base 
+    const NewTask = new pageTasks ({
+        taskName : req.body.task 
+    });
+    NewTask.save();
+
+    // adding task to page in data base 
+    PagesList.findOneAndUpdate(
+       { _id: currentPage}, 
+       { $push: { tasksList: [ {taskName: req.body.task} ] } },
+      function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+            }
+        });
     res.redirect("/");
 });
 app.post("/addPage",function(req,res){
@@ -60,6 +99,18 @@ app.post("/addPage",function(req,res){
             tasks:[]
         };
     pages.push(newPage);
+    pagesTitles.push(req.body.page);
+
+    // adding page to data base 
+    const newPageDB = new PagesList ({
+        _id : pages.length-1 ,
+        name:req.body.page ,
+    }) ;
+    newPageDB.save();
+
+    // change current page to new page
+    currentPage=pages.length-1;
+
     res.redirect("/");
 });
 app.get(`/changePage/:postID`,function (req,res) {
